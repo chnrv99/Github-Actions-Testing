551db0bf30ea2f247e14509fd41dbc78e4c36965
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-11-25 12:31:05 +0530

    updated

--- controller/points.js
+++ controller/points.js
@@ -3,10 +3,10 @@ exports.points = (req, res, next) => {
     // we will get points, prNumber, and username from post request in json format
     console.log(req.body);
     const points = req.body.points;
-    const prNumber = req.body.prNumber;
+    const issueID = req.body.issueID;
     const username = req.body.username;
     // now we will update the user's points in the database under mergedPRs, and increase the points
-    User.findOneAndUpdate({username: username}, {$inc: {points: points}, $push: {mergedPRs: {prNumber: prNumber, timestamp: Date.now(), points_receieved: points}}}, {new: true}, (err, user) => {
+    User.findOneAndUpdate({username: username}, {$inc: {points: points}, $push: {mergedPRs: {prNumber: issueID, timestamp: Date.now(), points_receieved: points}}}, {new: true}, (err, user) => {
         if (err) {
             console.log(err);
             res.json({success: false, message: "Error updating user's points"});
